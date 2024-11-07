e15fa20dd9853a68a2be99327d2b1b856e1309cb
latidoenlinea <latidoenlinea@gmail.com>
2024-11-07 12:08:26 -0600

    Update Controlador.js

--- JS/Controlador.js
+++ JS/Controlador.js
@@ -37,7 +37,7 @@ class Controlador {
         formData.append("frame", blob, "frame.jpg")
 
         try {
-            const response = await fetch("latidoenlinea.pythonanywhere.com", {
+            const response = await fetch("https://latidoenlinea.onrender.com/process_video", {
                 method: "POST",
                 body: formData
             })
